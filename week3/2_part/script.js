892de1fff9343ea05f2f36946b3e491f06d0914d
Madiyar <madiyarbekmurat@gmail.com>
2025-03-08 17:19:12 +0500

    LAB3

---
+++ week3/2_part/script.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("date").innerText = `Today's Date: ${new Date().toLocaleDateString()}`;
+});
+
+function addTask() {
+    const taskInput = document.getElementById("taskInput");
+    const taskText = taskInput.value.trim();
+
+    if (taskText === "") {
+        alert("Please enter a task!");
+        return;
+    }
+
+    const taskList = document.getElementById("taskList");
+
+    const li = document.createElement("li");
+    li.innerHTML = `
+        <span onclick="toggleComplete(this)">${taskText}</span>
+        <button class="delete-btn" onclick="removeTask(this)">X</button>
+    `;
+
+    taskList.appendChild(li);
+    taskInput.value = "";
+}
+
+function toggleComplete(task) {
+    task.classList.toggle("completed");
+}
+
+function removeTask(button) {
+    button.parentElement.remove();
+}
+
+function deleteAllTasks() {
+    document.getElementById("taskList").innerHTML = "";
+}
